1e57bc98e616cd7dbb7d4da2b83b1161570dbf25
JulienOza <julien.ozarowski@gmail.com>
2025-02-11 12:14:16 +0100

    màj exos

--- Exercices Ju/Exercice14.py
+++ Exercices Ju/Exercice14.py
@@ -1,6 +1,6 @@
 print('Ce programme ecrit pour un intervalle souhaité "Fizz" pour les nombres entiers multipes de 3, "Buzz" pour les multiples de 5, "FizzBuzz" pour les multiple de 3 et 5')
 
-for i in range (int(input("Veuillez saisir la limite basse de l'intervalle : ")),int(input("Veuillez saisir la limite haute de l'intervalle : "))) :
+for i in range (int(input("Veuillez saisir la limite basse de l'intervalle : ")),(int(input("Veuillez saisir la limite haute de l'intervalle : ")))+1) :
     if i%3 == 0 and i%5 == 0 :
         print("FizzBuzz")
     elif i%3 == 0 :
--- Exercices Ju/Exercice15.py
+++ Exercices Ju/Exercice15.py
@@ -1,7 +1,9 @@
-def full_name(firstName, lastName) :
-    return f"{firstName} {lastName}"
+def full_name(first_name, last_name) :
+    return f"{first_name} {last_name}"
 
-#print(f"Your full name is {fullName(input("What's your first name ? : "), input("What's your last name ? : "))}")
 
-fullName = full_name("John", "Doe")
-print(fullName)
+first_name = input("What's your first name ? : ")
+last_name = input("What's your last name ? : ")
+
+name = full_name(first_name, last_name).title()
+print(f"Your full name is {name}")
