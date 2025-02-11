84931b4984e853531cc988f301dc9b50d54a4af3
JulienOza <julien.ozarowski@gmail.com>
2025-02-11 15:28:09 +0100

    màj exos

--- Exercices Ju/Exercice15.py
+++ Exercices Ju/Exercice15.py
@@ -1,9 +1,10 @@
-def full_name(first_name, last_name) :
+# Si seul certains paramètres par défaut , les mettre en dernier 
+def full_name(first_name: str = "John", last_name: str = "Doe") : 
     return f"{first_name} {last_name}"
-
 
 first_name = input("What's your first name ? : ")
 last_name = input("What's your last name ? : ")
 
-name = full_name(first_name, last_name).title()
-print(f"Your full name is {name}")
+print(f"Your full name is {full_name(first_name, last_name).title()}")
+
+# print(f"Your full name is {full_name().title()}")
