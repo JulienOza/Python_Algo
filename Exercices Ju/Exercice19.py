05b2cc2c6d59c4d928fc9ded987b96d329145195
JulienOza <julien.ozarowski@gmail.com>
2025-02-12 15:07:50 +0100

    màj exos

--- Exercices Ju/Exercice19.py
+++ Exercices Ju/Exercice19.py
@@ -1,20 +1,20 @@
 def verification_adn(saisie: str) -> bool:
-    for char in saisie.lower():
-        if char not in "atcg":
+    for char in saisie.upper():
+        if char not in "ACTG":
             return False
     return True
 
-def saisie_adn(entree_sequence: str):
+def saisie_adn(entree_sequence: str) -> str:
     while not verification_adn(entree_sequence):
         print("Erreur : veuillez entrer une séquence valide.")
-        entree_sequence = input("Veuillez entrer une séquence ADN valide : ").lower()
+        entree_sequence = input("Veuillez entrer une séquence ADN valide : ").upper()
     return entree_sequence
 
-def proportion(chaine_adn: str, sequence_adn: str):
+def proportion(chaine_adn: str, sequence_adn: str) -> float:
     return round((chaine_adn.count(sequence_adn)) * len(sequence_adn) / len(chaine_adn) * 100, 2)
 
-chaine_adn = saisie_adn(input("Veuillez saisir une chaîne ADN : ").lower())
-sequence_adn = saisie_adn(input("Veuillez saisir une séquence ADN à rechercher dans la chaîne : ").lower())
+chaine_adn = saisie_adn(input("Veuillez saisir une chaîne ADN : "))
+sequence_adn = saisie_adn(input("Veuillez saisir une séquence ADN à rechercher dans la chaîne : ").upper())
 
 print(f"La séquence {sequence_adn} est présente dans {proportion(chaine_adn, sequence_adn)} % de la chaîne")
 
